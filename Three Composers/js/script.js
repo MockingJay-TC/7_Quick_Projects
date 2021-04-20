ab57b57e6a7beb6a8db2a9a415ce97e1873e619f
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-04-20 10:22:18 +0000

    3 added

--- Three Composers/js/script.js
+++ Three Composers/js/script.js
@@ -1,21 +1,21 @@
 let app = angular.module("ComposersList", []);
 app.controller("ComposerController",function($scope){
     $scope.pieces = [
-       {"pic": "assets/bach.jfif", "composer": "Bach", "title": "Goldberg Variations"},
-       {"pic": "assets/bach.jfif", "composer": "Bach", "title": "Harpsichord Concerto"},
-       {"pic": "assets/bach.jfif", "composer": "Bach", "title": "Brandenburg Concerto"},
-       {"pic": "assets/bach.jfif", "composer": "Bach", "title": "Air On G"},
-       {"pic": "assets/bach.jfif", "composer": "Bach", "title": "Contana #85"},
-       {"pic": "assets/beethoven.png", "composer": "Beethoven", "title": "Symphony #9"},
-       {"pic": "assets/beethoven.png", "composer": "Beethoven", "title": "Fur Elise"},
-       {"pic": "assets/beethoven.png", "composer": "Beethoven", "title": "Moonlight Sonata"},
-       {"pic": "assets/beethoven.png", "composer": "Beethoven", "title": "Violin Concerto"},
-       {"pic": "assets/beethoven.png", "composer": "Beethoven", "title": "Egnant Overture"},
-       {"pic": "assets/mozart.jfif", "composer": "Mozart", "title": "Eine Kleine Machtmusic"},
-       {"pic": "assets/mozart.jfif", "composer": "Mozart", "title": "Piano Concerto #21"},
-       {"pic": "assets/mozart.jfif", "composer": "Mozart", "title": "Don Giovanni"},
-       {"pic": "assets/mozart.jfif", "composer": "Mozart", "title": "Jupiter Symphony"},
-       {"pic": "assets/mozart.jfif", "composer": "Mozart", "title": "larinet Concerto #3"}
+       {"pic": "images/bach.jfif", "composer": "Bach", "title": "Goldberg Variations"},
+       {"pic": "images/bach.jfif", "composer": "Bach", "title": "Harpsichord Concerto"},
+       {"pic": "images/bach.jfif", "composer": "Bach", "title": "Brandenburg Concerto"},
+       {"pic": "images/bach.jfif", "composer": "Bach", "title": "Air On G"},
+       {"pic": "images/bach.jfif", "composer": "Bach", "title": "Contana #85"},
+       {"pic": "images/beethoven.png", "composer": "Beethoven", "title": "Symphony #9"},
+       {"pic": "images/beethoven.png", "composer": "Beethoven", "title": "Fur Elise"},
+       {"pic": "images/beethoven.png", "composer": "Beethoven", "title": "Moonlight Sonata"},
+       {"pic": "images/beethoven.png", "composer": "Beethoven", "title": "Violin Concerto"},
+       {"pic": "images/beethoven.png", "composer": "Beethoven", "title": "Egnant Overture"},
+       {"pic": "images/mozart.jfif", "composer": "Mozart", "title": "Eine Kleine Machtmusic"},
+       {"pic": "images/mozart.jfif", "composer": "Mozart", "title": "Piano Concerto #21"},
+       {"pic": "images/mozart.jfif", "composer": "Mozart", "title": "Don Giovanni"},
+       {"pic": "images/mozart.jfif", "composer": "Mozart", "title": "Jupiter Symphony"},
+       {"pic": "images/mozart.jfif", "composer": "Mozart", "title": "larinet Concerto #3"}
     ];
 });
 
